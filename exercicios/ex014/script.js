ce457442cf62798cdfac6224bbcbcbdf11eec780
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-07-07 15:22:43 -0300

    att

---
+++ exercicios/ex014/script.js
@@ -0,0 +1,25 @@
+
+    var agora = new Date()
+    var hora = agora.getHours()
+    var txth = window.document.querySelector('#horario')
+    var img = window.document.querySelector('#imghtml')
+
+    var hora = 11
+    function load() {
+        if(hora < 12){
+            txth.innerHTML  = `Agora são <strong>${hora}h</strong>, bom dia!`
+        } else {
+            if(hora < 18){
+                txth.innerHTML  = `Agora são <strong>${hora}h</strong>, boa tarde!`
+                document.body.style.background = '#e2916c'
+                img.src = 'imagens/tarde.jpg'
+                
+                
+            } else{
+                txth.innerHTML  = `Agora são <strong>${hora}h</strong>, boa noite!`
+                document.body.style.background = '#0d0d44'
+                img.src = 'imagens/noite.jpg'
+            }
+        }
+    }
+
---
+++ exercicios/ex015/script.js
@@ -0,0 +1,30 @@
+function clicar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('#inpt')
+    var res = document.querySelector('#res')
+    
+    if(fano.value <= 0 || fano.value > ano) {
+        window.alert('error301')
+    } else {
+        var idade = ano - fano.value
+        var fsex = document.getElementsByName('radsex')
+
+        if(fsex[0].checked) {
+            var genero = 'Homem'
+        } else {
+            var genero = 'Mulher'
+        }
+
+        if(idade >= 0 && idade < 10){
+            var epoca = 'criança'
+        } else if(idade <= 22){
+            var epoca = 'jovem'
+        } else if(idade <= 55){
+            var epoca = 'adulto(a)'
+        } else {
+            var epoca = 'idoso(a)'
+        }
+    }
+        res.innerHTML = `Detectei ${genero} ${epoca} de ${idade} anos` 
+}
