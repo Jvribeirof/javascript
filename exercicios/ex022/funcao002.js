21c58b6e238804f88b86e135a2c7f2ad2b6966e5
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-07-13 13:05:58 -0300

    att

---
+++ exercicios/ex022/funcao002.js
@@ -0,0 +1,8 @@
+function cal(a,b,c,x){
+    let form = a*(x**2) + b*x + c
+    return form
+}
+
+var teste = cal(1,4,4,6)
+
+console.log(teste)
--- exercicios/ex024/object.js
+++ exercicios/ex024/object.js
@@ -1,1 +1,2 @@
-let amigo = {} 
+let amigo = {nome:'joao',idade:'19', sexo:'m'}
+console.log(amigo.idade) 
