7d4bdad824919e93bab9e03edc68b32419aebe21
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-07-24 16:01:54 -0300

    att

--- exercicios/ex030/filtro.js
+++ exercicios/ex030/filtro.js
@@ -1,19 +1,24 @@
 let list = document.querySelectorAll('.filter');
 let itens = document.querySelectorAll('.item');
 
+//SELECINAR
 for(let i = 0; i < list.length; i++){
     list[i].addEventListener('click',function(){
         for(let j = 0; j < list.length; j++){
             list[j].classList.remove('active');
         }
         list[i].classList.add('active');
-        let dataFilter = list[i].getAttribute('data-filter')
+
+//SELECIONAR ITENS
+       let dataFilter = list[i].getAttribute('data-filter')
 
         for(let k = 0; k < itens.length; k++){
+            //REMOVE TODOS OS ITENS DA TELA
             itens[k].classList.remove('item-active');
             itens[k].classList.add('item-hide');
 
-            if(itens[k].getAttribute(data-item) == dataFilter || itens[k].getAttribute(data-item) == "todos"){
+            //RE-ADICIONA APENAS OS QUE SÃO DO FILTRO
+            if(itens[k].getAttribute('data-item') == dataFilter || dataFilter == 'todos'){
                 itens[k].classList.remove('item-hide');
                 itens[k].classList.add('item-active');
             }
