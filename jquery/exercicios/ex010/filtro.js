42df2f7a50ce9230b154a8048b0ea74bf0e5f094
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-08-03 14:06:43 -0300

    att

--- jquery/exercicios/ex010/filtro.js
+++ jquery/exercicios/ex010/filtro.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     let list = $('.filter');
     let itens = $('.item');
+    let cont = 0
+    let vet = []
+    let vet2 = []
+
+    function verificar(x){
+        let retorno = $(x).attr('class') == 'filter active'
+        return retorno
+    }
 
     //SELECINAR
     for(let i = 0; i < list.length; i++){
@@ -11,20 +19,28 @@ $(document).ready(function(){
             } else {
                 $(list[0]).removeClass('active');
                 $(list[i]).toggleClass('active');
+                vet[i] = $(list[i]).attr('data-filter');
             }
+
+            console.log(vet)
 
             //SELECIONAR ITENS
             let dataFilter = $(list[i])
-
-            var id = $(dataFilter).attr('id');
-            if(id == 'todos'){
+            if($(dataFilter).attr('data-filter') == 'todos'){
                 $(itens).show();
+                vet = []
             } else{
-                //if(SE ALGUM FILTRO ESTIVER SELECIONADO, PULAR O IF){
-                    $(itens).hide();
-                    //$(itens).css('display') != 'none'
+                //if($(itens).css('display') == 'none'){
+                    $(itens).toggle();
+                    //$(itens).css('display') == 'none'
                 //}
-                $(itens).filter('.f-'+id).toggle();
+                for(let j = 0; j < vet.length; j++){
+                    $(itens).filter('.f-'+vet[j]).toggle();
+                }
+                for(let k = 1; k < list.length; k++){
+                   vet2[k] = verificar(list[k])
+                }
+                console.log(vet2)
             }
         })
     }
