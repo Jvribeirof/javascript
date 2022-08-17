37da55f0da550eb65b5b3c4714ee9bb11ace8503
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-08-17 18:28:46 -0300

    att
    att

--- jquery/exercicios/ex013/teste.js
+++ jquery/exercicios/ex013/teste.js
@@ -2,5 +2,12 @@ let teste = [0,1,2,3,4,5,6,7]
 let aux = []
 
 delete teste[4]
-//teste = [0,1,2,3,null,5,6,7]
-console.log(teste)
+delete teste[3]
+
+teste.sort();
+for(let i = teste.length-1; i >= 0; i--){
+    if(teste[i] == null){
+        teste.pop();
+    }
+}
+console.log(teste);
